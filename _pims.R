541a21f3c62d8346b91d7062a0d02d45d791d9f0
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2022-07-19 13:02:20 +0300

    calculation raw_pims without repeat

---
+++ _pims.R
@@ -0,0 +1,73 @@
+#### Написание и тестирование функции pims
+library(dplyr, warn.conflicts = FALSE)
+library(tidyr, warn.conflicts = FALSE)
+library(data.table, warn.conflicts = FALSE)
+library(purrr, warn.conflicts = FALSE)
+
+pbp <- data.table::fread("./data/nbastats_2021.csv")
+shots <- data.table::fread("./data/shotdetail_2021.csv")
+player_info <- data.table::fread("./data/commonal_players.csv")
+
+### Обработка входных данных (удаление дубликатов, удаление ненужных полей и бросков с дальней дистанции, трансформация дистанции броска в футы от кольца)
+transform_pbp <- unique(pbp[, .(GAME_ID, EVENTNUM, EVENTMSGTYPE, PERSON1TYPE, PLAYER1_ID, PLAYER1_TEAM_ID, PERSON2TYPE, PLAYER2_ID, PLAYER2_TEAM_ID,
+                         PERSON3TYPE, PLAYER3_ID, PLAYER3_TEAM_ID, PLAYER1, PLAYER2, PLAYER3, PLAYER4, PLAYER5, PLAYER6, PLAYER7,
+                         PLAYER8, PLAYER9, PLAYER10)])
+transform_shots <- shots[LOC_Y <= 297.5, .(GAME_ID, GAME_EVENT_ID, PLAYER_ID, TEAM_ID, LOC_X, LOC_Y)]
+transform_shots$LOC_X <- transform_shots$LOC_X / 10
+transform_shots$LOC_Y <- transform_shots$LOC_Y /10 + 5.25
+
+### Находим игроков, которые сделали в два раза больше минимально необходимого количества бросков для одной группы за одну команду в сезоне
+min_shots <- 200
+players <- transform_shots[, .(.N), by=.(PLAYER_ID, TEAM_ID)][N >= min_shots*2, .(PLAYER_ID, TEAM_ID)]
+
+### Переходим на расчёт каждого игрока в отдельности
+player <- players[1]
+
+### Получение строк в transform_pbp, когда игрок совершал бросок и объединение их с transform_shots
+row_player_shot <- transform_pbp[PLAYER1_ID == player$PLAYER_ID & PLAYER1_TEAM_ID == player$TEAM_ID & EVENTMSGTYPE %in% c(1, 2)]
+player_shot_data <- data.table::merge.data.table(row_player_shot, transform_shots, by.x = c("GAME_ID", "EVENTNUM"), by.y = c("GAME_ID", "GAME_EVENT_ID"))
+
+### Преобразование данных wide-to-long, где строка - это данные о событии в игре для каждого из пяти игроков команды, в которой играет переменная player
+team_on_court <- data.table::melt(player_shot_data[, .(GAME_ID, EVENTNUM, PERSON1TYPE, LOC_X, LOC_Y, PLAYER1, PLAYER2, PLAYER3, PLAYER4, PLAYER5, 
+                                           PLAYER6, PLAYER7, PLAYER8, PLAYER9, PLAYER10)],
+                      id.vars = c("GAME_ID", "EVENTNUM", "PERSON1TYPE", "LOC_X", "LOC_Y"),
+                      measure.vars = c("PLAYER1", "PLAYER2", "PLAYER3", "PLAYER4", "PLAYER5", "PLAYER6", "PLAYER7", "PLAYER8", "PLAYER9", "PLAYER10"),
+                      value.name = "PLAYER_ID")[
+                        (PERSON1TYPE == 4 & variable %in% c("PLAYER6", "PLAYER7", "PLAYER8", "PLAYER9", "PLAYER10")) 
+                        | (PERSON1TYPE == 5 & variable %in% c("PLAYER1", "PLAYER2", "PLAYER3", "PLAYER4", "PLAYER5"))                       
+                      ][, c("variable", "PERSON1TYPE")  := NULL]
+### Фильтрация партнёров, не проходящих по условию минимального количества бросков в группе (N >= min_shots & N <= cnt_shots - min_shots)
+teammates <- team_on_court[, .(.N), by="PLAYER_ID"][N >= min_shots & N <= dim(player_shot_data)[1] - min_shots, PLAYER_ID]
+
+### Переходим на расчёт каждого партнёра в отдельности
+teammate <- teammates[1]
+
+### Создаём фильтрацию team_on_court с данными только двух игроков. 
+### Разделяем броски на две категории: on(когда player и teammate на площадке) и off(когда на площадке только player)
+player_teammate_shot <- team_on_court[PLAYER_ID %in% c(player$PLAYER_ID, teammate)]
+factor_table <- player_teammate_shot[, .(.N), by=c("GAME_ID", "EVENTNUM")][, ("ON_OFF") := data.table::fifelse(N == 2, "on", "off")]
+
+### Добавляем информацию ON_OFF в таблицу player_teammate_shot, удаляем  дубликаты строк
+player_teammate_factor <- unique(data.table::merge.data.table(player_teammate_shot, factor_table, by=c("GAME_ID", "EVENTNUM"))[
+  , "PLAYER_ID" := NULL])[, .(ON_OFF, LOC_X, LOC_Y)]
+
+min_group <- player_teammate_factor[, .N, by="ON_OFF"][, min(N)]
+
+### Расчёт Raw_pims (без учёта случайных колебаний на таком количестве бросков)
+### Деление на группы обрезка большей группы до размера наименьшей.
+set.seed(42)
+on_shots <- player_teammate_factor[ON_OFF == "on"][sample(.N, min_group)]
+off_shots <- player_teammate_factor[ON_OFF == "off"][sample(.N, min_group)]
+
+### Разделение на четыре вектора по парам (координата, on_off)
+off_x <- off_shots[, LOC_X]
+on_x <- on_shots[, LOC_X]
+off_y <- off_shots[, LOC_Y]
+on_y <- on_shots[, LOC_Y]
+
+### СРедние координаты выборки по модулю
+mean_x <- mean(abs(c(off_x, on_x)))
+mean_y <- mean(c(off_y, on_y))
+
+raw_pims <- sum(abs(sort(off_x) - sort(on_x))/sqrt(mean_x) + abs(sort(off_y) - sort(on_y))/sqrt(mean_y))/min_group
+print(raw_pims)
