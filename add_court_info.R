d8bd6a9401f9122b5e10ff88224b6f91b6be364f
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2023-03-30 16:56:22 +0300

    add plot and player_on_court in main

---
+++ add_court_info.R
@@ -0,0 +1,28 @@
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(data.table)
+library(hoopR)
+
+source("utils.R")
+
+setwd("./data/")
+
+get_nba_data(seasons = seq(2022, 2022), data = c("nbastats", "shotdetail"), untar = TRUE)
+
+setwd("../")
+
+df <- read.csv("./data/nbastats_2022.csv")
+
+df_on_court <- rbindlist(lapply(unique(df$GAME_ID), function(game_id){
+  tmp_df <- df[df$GAME_ID == game_id,]
+  if(unique(tmp_df$GAME_ID)[1] == 22200234){
+    tmp_df <- tmp_df[tmp_df$EVENTNUM != 248,]
+  }
+  tmp_df <- players_on_court(tmp_df)
+  return(tmp_df)
+}))
+
+fwrite(df_on_court, "./data/nbastats_2022_on_court.csv")
